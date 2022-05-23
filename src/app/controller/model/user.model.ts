be8da831c27d8357b76fe53c07933c3ff8ed1c45
Hp <mohamed.kiddi.1@gmail.com>
2022-05-23 13:40:59 +0100

    .

--- src/app/controller/model/user.model.ts
+++ src/app/controller/model/user.model.ts
@@ -4,7 +4,7 @@ import {ExpressionBesoin} from "./expression-besoin.model";
 export class User {
   public id:number;
   public ref:string;
-  public userName:string;
+  public username:string;
   public serviceDemandeur=new ServiceDemandeur();
   public expressionBesoins =new Array<ExpressionBesoin>();
 }
